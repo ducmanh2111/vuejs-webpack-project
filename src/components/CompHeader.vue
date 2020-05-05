fcb86ca0b3c57f8ffeab6d3dbd4e109faf7b40ae
ducmanh2111 <ducmanh2111@gmail.com>
2020-05-06 02:05:40 +0700

    Vuejs28_Truyen-event-up-su-dung-emit

--- src/components/CompHeader.vue
+++ src/components/CompHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <header>
-    <h1>{{ title }}</h1>
+    <h1>{{ title }} <br>
+      <button @click="changeTitleHeader($event)">Thay đổi title từ CompHeader.vue</button>
+    </h1>
   </header>
 </template>
 
@@ -12,6 +14,23 @@ export default {
   },
   data() {
     return {
+    }
+  },
+  methods: {
+    changeTitleHeader(e) {
+      // Không được phép thay đổi trực tiếp props từ component cha 
+      // -> Truyền sự kiện thông báo ra ngoài 
+      // -> Kích hoạt sự kiện 'changeTitleEvent' trong CompHeader.vue
+      // -> Khi kích hoạt function bên ngoài App.vue nó sẽ chạy
+      // -> Khi function đó dược chạy -> Thay đổi dữ liệu 
+      console.log('Trước khi kích hoạt sự kiện changeTitleEvent trong trong CompHeader.vue');
+      var data = {
+        title: "test -> data truyền thông qua biến event "
+      }
+
+      this.$emit('changeTitleEvent', data);
+      console.log('Sau khi sự kiện changeTitleEvent trong trong CompHeader.vue xong');
+      // console.log(this.title);
     }
   }
 }
