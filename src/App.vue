8949f0e59829a8659b6b6e38edb1c801fa7cc516
ducmanh2111 <ducmanh2111@gmail.com>
2020-05-06 00:41:55 +0700

    Vuejs24_Cach-tao-component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+    <HelloWorld />
+    <comp-header />
+    <list-products />
+    <comp-footer />
   </div>
 </template>
 
 <script>
+/*
+App
+  Header
+  ListProducts
+    Product
+    Product
+    Product
+  Footer
+*/
+
 import HelloWorld from './components/HelloWorld'
+import CompHeader from './components/CompHeader'
+import CompFooter from './components/CompFooter'
+import ListProducts from './components/ListProducts'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CompHeader,
+    ListProducts,
+    CompFooter
   }
 }
 </script>
---
+++ src/components/ListProducts.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list-products">
+    <ul>
+      <li v-for="item in listProducts" v-bind:key="item.id"> {{ item.color }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list-products',
+  data() {
+    return {
+      listProducts: [
+        {
+          id: 1,
+          color: "Vàng"
+        },
+        {
+          id: 2,
+          color: "Xanh"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
